b9a7de9531a9ad37e71798036306e14deb65e3f5
maxbittker <maxbittker@gmail.com>
2018-08-08 02:30:04 -0400

    floyd steinburg!

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -29,6 +29,7 @@ vec2 hash(vec2 p) {
 }
 
 vec2 pixel = vec2(1.0) / resolution;
+vec2 vp = vec2(1.0) / vec2(min(videoResolution.x, videoResolution.y));
 vec3 voronoi(in vec2 x) {
   vec2 n = floor(x);
   vec2 f = fract(x);
@@ -201,11 +202,20 @@ vec4 dither8x8(vec2 position, vec4 color) {
   return vec4(color.rgb * dither8x8(position, length(color)), 1.0);
 }
 
+const int lookupSize = 32;
+const float errorCarry = 0.10;
+float getGrayscale(vec2 coords) {
+  // vec2 uv = coords / resolution.xy;
+  // uv.y = 1.0 - uv.y;
+  vec3 sourcePixel = texture2D(webcam, clamp(coords, 0., 1.0)).rgb;
+  return luma(sourcePixel);
+}
 void main() {
-  vec2 pos = squareFrame(resolution) * 00.5;
+  vec2 pos = squareFrame(resolution);
   vec3 color;
   vec2 webcamCoord = (uv * 0.5 + vec2(0.5)) * resolution / videoResolution;
   vec2 flipwcord = vec2(1.) - webcamCoord;
+  flipwcord.x = webcamCoord.x;
   vec2 eye1 = eyes[0] / videoResolution;
   vec2 eye2 = eyes[1] / videoResolution;
   vec2 textCoord = uv * 0.5 + vec2(0.5);
@@ -213,8 +223,6 @@ void main() {
       distance(eye1, flipwcord) < distance(eye2, flipwcord) ? eye1 : eye2;
   vec2 suck = -2.0 * pixel * normalize(flipwcord - closeEye) *
               random(uv + t * vec2(1.0));
-  // *(0.5 + noise(vec3(uv * 10., t * 5.)));
-
   vec3 webcamColor = texture2D(webcam, flipwcord).rgb * 0.95;
   float ed = smin(distance(eye1, flipwcord), distance(eye2, flipwcord), 0.01);
 
@@ -223,125 +231,30 @@ void main() {
   float s = 0.1 * ifd * 4.;
   vec2 mid = (eye1 + eye2) * 0.5;
 
-  color = vec3(0.09, 0.089, 0.087);
-
-  vec3 red = vec3(0.9, 0.25, 0.);
-  vec3 yellow = vec3(0.85, 0.7, 0.);
-  vec3 brown = vec3(0.4, 0.2, 0.1);
-  // vec3 hue = red;
-  float m = 100.;
-  // pos += noise(vec3(pos * 15., 0.5)) * 0.01;
-  vec3 c = voronoi((40.0 + 5. * noise(vec3(pos * 0.1, 0.))) * pos);
-
-  // colorize
-  vec3 col = red;
-  float h = 0.;
-  if (c.z < 1.2) {
-    h = 1.;
-  }
-  if (c.z < 0.7) {
-    h = 2.;
-  }
-  // col += c.x * 0.5;
-  // vec3 col = hsv2rgb(vec3(hue, 0.4, 0.6));
-  // 0.5 + 0.5 * cos(c.y * 6.2831 + vec3(0.0, 1.0, 2.0));
-  // col *= clamp(1.0 - 0.4 * c.x * c.x, 0.0, 1.0);
-  // col -= (1.0 - smoothstep(0.08, 0.09, c.x));
-  col = hsv2rgb(vec3(0.0, 1.0, 0.9));
-  if (c.z < 1.2) {
-    col = hsv2rgb(vec3(0.1, 0.9, 0.9));
-  }
-  if (c.z < 0.7) {
-    col = hsv2rgb(vec3(0.05, 0.9, 0.35));
-  }
-  vec2 spos = pos += noise(vec3(pos * 1., 0.5 + t * 0.001)) * 0.5;
-  // spos *= 0.1;
-  float dilation =
-      noise(vec3((spos * vec2(1.0, 5.)) + vec2(0., t * 0.01) + (vec2(0.3) * h),
-                 t * 0.01)) +
-      0.3;
-
-  // dilation = 1.0 - abs(length(webcamColor - col));
-  float dd = (c.y - c.x) * (1.0 - c.x * 1.0);
-  dd *= dilation;
-  dd *= 1.5;
-
-  col = hsv2rgb(vec3(0.6, 1.0 - (dilation * 0.3) - c.x * 0.2, 0.7));
-  if (c.z < 1.2) {
-    col = hsv2rgb(vec3(0.2, 0.6 - dilation * 0.2, 0.9));
-  }
-  if (c.z < 0.7) {
-    col = hsv2rgb(vec3(0.1, 0.9, 0.85));
+  float xError = 0.0;
+  for (int xLook = 0; xLook < lookupSize; xLook++) {
+    float grayscale =
+        getGrayscale(flipwcord + vec2(-lookupSize + xLook, 0) * vp);
+    grayscale += xError;
+    float bit = grayscale >= 0.5 ? 1.0 : 0.0;
+    xError = (grayscale - bit) * errorCarry;
   }
 
-  if (dd > 0.4 || c.x < 0.05) {
-    color = col;
-  } else {
-    color += col * 0.001;
+  float yError = 0.0;
+  for (int yLook = 0; yLook < lookupSize; yLook++) {
+    float grayscale =
+        getGrayscale(flipwcord + vec2(0, -lookupSize + yLook) * vp);
+    grayscale += yError;
+    float bit = grayscale >= 0.5 ? 1.0 : 0.0;
+    yError = (grayscale - bit) * errorCarry;
   }
-  // color = (c.y - c.x) * col;
-  // vec3 cel = cellular(pos * 5.);
-  // color = hsv2rgb(
 
-  //     vec3(cel.z / 9., 0.5, 0.5));
-  // if (cel.x < 0.05) {
-  //   color = vec3(0.);
-  // }
-  // vec3 col = hsv2rgb(vec3(t + floor(c.y * 1.5) / 5., 0.5, 0.5));
+  float finalGrayscale = getGrayscale(flipwcord);
+  finalGrayscale += xError * 0.5 + yError * 0.5;
+  float finalBit = finalGrayscale >= 0.5 ? 1.0 : 0.0;
+  gl_FragColor = vec4(finalGrayscale, finalGrayscale, finalGrayscale, 1);
 
-  // if (pos.x < -1. + pixel.x * 5.) {
-  //   if (mod(t, 20.) > 18.) {
-  //     // color = vec3(1., 1., 1.);
-  //   } else {
-  //     // color = vec3(0.2, 0.3, 0.4);
-  //   }
-  // } else {
-  //   float weight = luma(webcamColor);
+  // gl_FragColor = vec4(finalBit, finalBit, finalBit, 1);
 
-  //   float edge = luma(texture2D(webcam, flipwcord - pixel.x).rgb) -
-  //                luma(texture2D(webcam, flipwcord + pixel.x).rgb);
-
-  //   edge *= 3.;
-
-  //   if (distance(mid.x, flipwcord.x) < (ifd * 1.0) &&
-  //       mid.y - flipwcord.y < ifd * 1.8 && mid.y - flipwcord.y > ifd *
-  //       -2.5)
-  //       {
-  //   } else {
-  //     weight *= 0.8;
-  //   }
-  //   vec2 fall = 2. * pixel * vec2(1., 0) * (0.5 + random(vec2(uv.y, t)) *
-  //   0.5) *
-  //               (1. - weight);
-
-  //   color = texture2D(backBuffer, textCoord - fall).rgb * 1.0;
-  //   if (uv.y < 0.0) {
-  //     // color =weight * vec3(1.0);
-  //   }
-  // }
-
-  // if (ed < s) {
-  // distance(mid,flipwcord)<0.2
-  // if (distance(mid.x, flipwcord.x) < (ifd * 1.0) &&
-  //     mid.y - flipwcord.y < ifd * 1.8 && mid.y - flipwcord.y > ifd * -2.5)
-  //     {
-  //   float weight = luma(webcamColor);
-  //   // color = weight * vec3(1.0);
-  // }
-  // if (luma(webcamColor) <
-  //  0.4) {
-  // 0.5 + 0.2 * noise(vec3(uv * 200., t * 0.1))) {
-  // color = vec3(1.);
-  // color *= 0.25;
-  // }
-  vec2 st = gl_FragCoord.xy / resolution.xy;
-  st.x *= resolution.x / resolution.y;
-  vec3 rgb = webcamColor;
-  rgb += vec3(1.0) * t * 0.01 - ed;
-  ;
-  // * st.y + eye1.x * 10.; // t * -0.01;
-  rgb = mod(rgb, 1.0);
-  vec3 drgb = dither8x8(gl_FragCoord.xy / 2., rgb);
-  gl_FragColor = vec4(drgb, 1.0);
-  // gl_FragColor = vec4(color, 1.0);
+  // gl_FragColor = vec4(vec3(1.0) * getGrayscale(flipwcord), 1.0);
 }

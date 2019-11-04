e3ea9343d4c34d46f433a9631853d71ffe743676
max bittker <maxbittker@gmail.com>
2019-11-04 10:30:50 -0500

    idk

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -5,9 +5,6 @@ uniform sampler2D backBuffer;
 uniform sampler2D webcam;
 uniform vec2 videoResolution;
 uniform vec2 scaledVideoResolution;
-
-uniform vec4 bands;
-
 varying vec2 uv;
 
 // clang-format off
@@ -17,17 +14,9 @@ varying vec2 uv;
 // clang-format on
 #define PI 3.14159265359
 
-float random(in vec2 _st) {
-  return fract(sin(dot(_st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
-}
-vec2 hash(vec2 p) {
-  p = vec2(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5, 183.3)));
-  return fract(sin(p) * 18.5453);
-}
-
 vec2 pixel = vec2(1.0) / resolution;
 vec2 vp = vec2(1.0) / vec2(min(videoResolution.x, videoResolution.y));
-float maxd(vec2 i) { return max(abs(i.x), abs(i.y)); }
+
 void main() {
   vec2 uvN = (uv * 0.5) + vec2(0.5);
   vec2 resRatio = scaledVideoResolution / resolution;
